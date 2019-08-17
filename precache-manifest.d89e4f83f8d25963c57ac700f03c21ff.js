82ecc369600eb5ced479189089698338cb7c97c6
Sabuj Ali <prosabuj98@gmail.com>
2019-08-17 19:16:00 +0600

    Updates

---
+++ precache-manifest.d89e4f83f8d25963c57ac700f03c21ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23ed892acd6df9ec989dd21165dedc4",
+    "url": "/unsplash-app/index.html"
+  },
+  {
+    "revision": "ec5e791b97e0f1c44511",
+    "url": "/unsplash-app/static/css/main.dedf8c0f.chunk.css"
+  },
+  {
+    "revision": "58f066c50bc8910b1539",
+    "url": "/unsplash-app/static/js/2.15ec95c8.chunk.js"
+  },
+  {
+    "revision": "ec5e791b97e0f1c44511",
+    "url": "/unsplash-app/static/js/main.b155e990.chunk.js"
+  },
+  {
+    "revision": "13f3a1abba2ced3fb2c2",
+    "url": "/unsplash-app/static/js/runtime~main.66a63806.js"
+  },
+  {
+    "revision": "9a8a685c7772fa57b045580d7307fdfd",
+    "url": "/unsplash-app/static/media/banner.9a8a685c.jpg"
+  },
+  {
+    "revision": "86d2072d82eb878ced537482d542c7cb",
+    "url": "/unsplash-app/static/media/location.86d2072d.svg"
+  },
+  {
+    "revision": "405675b4665e9fe476c4aeac6d7150c0",
+    "url": "/unsplash-app/static/media/web.405675b4.svg"
+  }
+]);
